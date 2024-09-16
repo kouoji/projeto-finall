81d5ba2702f435c7cde066f303654f21bd4c4473
Alessandro-cesf <116165893+kouoji@users.noreply.github.com>
2024-09-16 10:53:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-let area = document.getElementById("area");
-function entrar() {
-  let nome = prompt("Digite seu nome");
-  return (area.innerHTML = `Bem vindo ${nome}.`);
-}
+var lista = ["alunos1", "alunos2", "alunos3", 100];
 
-let botaoSair = document.createElement("button");
+//buscando elemento pela posição dentro do array
+console.log(lista);
+console.log(lista[3]);
+
+//descobrindo o tamanho do array
+console.log(lista.length);
+
+//saber se um elemento esta dentro de um array
+console.log(lista.indexOf("alunos3"));
+console.log(lista.indexOf("alunos4"));
+
+//alternando elemento dentro do array
+lista[0] = "Asdrubal";
+console.log(lista);
+
+//adcionar um elemento no array
+lista.push("alunos4");
+console.log(lista);
+
+//removendo o primeiro elemento da lista
+lista.shift();
+console.log(lista);
+
+//removendo o ultimo elemento da lista
+lista.pop();
+console.log(lista);
+
+//retornando os elementos de lista com separador
+lista.join();
+console.log(lista.join("-"));
