af5e1fc25728431c073b507d2c48379c6adc9e7a
Alessandro-cesf <116165893+kouoji@users.noreply.github.com>
2024-09-25 08:59:07 -0300

    Add files via upload

---
+++ AULA09_PROJETO_OBJETO/script.js
@@ -0,0 +1,13 @@
+//criar uma variável do tipo objeto
+let pessoa = {
+  nome: "Alessandro",
+  sobrenome: "Silva",
+  idade: 19,
+  altura: 1.67,
+  cargo: "aluno",
+};
+
+//consumindo o objeto todo
+console.log(pessoa);
+//consumindo uma das características do objeto
+console.log(pessoa.nome);
